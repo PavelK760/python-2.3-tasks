6fc88b56899f9bebdb445af76c8db6407598586e
PavelK760 <104727697+PavelK760@users.noreply.github.com>
2022-12-29 22:54:53 +0500

    Doctest (line 54 to 79)

--- task_2_3_2(1).py
+++ task_2_3_2(1).py
@@ -54,9 +54,9 @@ class DataSet:
     def increment_dict(dictionary, key, amount):
         """Увеличивает словарь на определённое количество"""
         """Увеличивает словарь на определённое количество
-        >>> DataSet.increment_dict({'a': 1, 'b': 2, 'c': 3}, 'a', 6){{'a': 7, 'b': 2, 'c': 3}}
-        >>> DataSet.increment_dict({'a': 1, 'b': 2, 'c': 3}, 'b', 6){{'a': 1, 'b': 8, 'c': 3}}
-        >>> DataSet.increment_dict({'a': 1, 'b': 2, 'c': 3}, 'c', 6){{'a': 1, 'b': 2, 'c': 9}}
+         >>> DataSet.increment_dict({'a': 1, 'b': 2, 'c': 3}, 'a', 6){{'a': 7, 'b': 2, 'c': 3}}
+         >>> DataSet.increment_dict({'a': 1, 'b': 2, 'c': 3}, 'b', 6){{'a': 1, 'b': 8, 'c': 3}}
+         >>> DataSet.increment_dict({'a': 1, 'b': 2, 'c': 3}, 'c', 6){{'a': 1, 'b': 2, 'c': 9}}
         """
         if key in dictionary:
             dictionary[key] += amount
@@ -69,9 +69,9 @@ class DataSet:
 
         :returns dict
 
-        >>> DataSet.get_average({1: [2, 5], 2: [3, 6]}){1: 3, 2: 4}
-        >>> DataSet.get_average({1: [3, 3], 2: [4, 10]}){1: 3, 2: 7}
-        >>> DataSet.get_average({1: [2, 3], 2: [0, 16]}){1: 2, 2: 8}
+         >>> DataSet.get_average({1: [2, 5], 2: [3, 6]}){1: 3, 2: 4}
+         >>> DataSet.get_average({1: [3, 3], 2: [4, 10]}){1: 3, 2: 7}
+         >>> DataSet.get_average({1: [2, 3], 2: [0, 16]}){1: 2, 2: 8}
         """
         new_dictionary = {}
         for key, values in dictionary.items():
